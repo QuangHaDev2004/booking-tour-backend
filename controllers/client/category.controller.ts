be259419427102133b23c8556bd2ce1686c5cc46
QuangHaDev2004 <ngoquangha2004@gmail.com>
2025-11-26 16:53:51 +0700

    feat: category api

--- controllers/client/category.controller.ts
+++ controllers/client/category.controller.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from "express";
 import Category from "../../models/category.model";
+import * as categoryHelper from "../../helpers/category.helper";
+import Tour from "../../models/tour.model";
+import City from "../../models/city.model";
+import moment from "moment";
 
 export const list = async (req: Request, res: Response) => {
   try {
@@ -42,9 +46,67 @@ export const list = async (req: Request, res: Response) => {
       avatar: categoryDetail.avatar,
     });
 
+    // Category Detail
+    const categoryFinal = {
+      name: categoryDetail.name,
+      description: categoryDetail.description,
+    };
+
+    // Tour List by Category
+    const categoryId = categoryDetail.id;
+    const categoryChild = await categoryHelper.getCategoryChild(categoryId);
+    const categoryChildId = categoryChild.map((item: any) => item.id);
+
+    const tourList = await Tour.find({
+      category: {
+        $in: [categoryId, ...categoryChildId],
+      },
+      deleted: false,
+      status: "active",
+    }).sort({
+      position: "desc",
+    });
+
+    const tourListFinal = [];
+    for (const item of tourList) {
+      const itemFinal = {
+        id: item.id,
+        name: item.name,
+        avatar: item.avatar,
+        time: item.time,
+        slug: item.slug,
+        priceNewAdult: item.priceNewAdult,
+        locationsFromName: [],
+        departureDateFormat: "",
+        discount: 0,
+      };
+
+      if (item.priceAdult && item.priceNewAdult) {
+        itemFinal.discount = Math.floor(
+          ((item.priceAdult - item.priceNewAdult) / item.priceAdult) * 100
+        );
+      }
+
+      if (item.locationsFrom.length > 0) {
+        itemFinal.locationsFromName = await City.find({
+          _id: { $in: item.locationsFrom },
+        });
+      }
+
+      if (item.departureDate) {
+        itemFinal.departureDateFormat = moment(item.departureDate).format(
+          "DD/MM/YYYY"
+        );
+      }
+
+      tourListFinal.push(itemFinal);
+    }
+
     res.status(200).json({
       message: "Danh sách tour theo danh mục!",
       breadcrumb,
+      categoryDetail: categoryFinal,
+      tourList: tourListFinal,
     });
   } catch (error) {
     console.log("Lỗi khi gọi list", error);
